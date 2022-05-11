b224376eb80e3d2a59fc361a5629717c0765af6e
Hugo Cabaret <hugo.cabaret@openlimits.pt>
2022-05-11 17:27:55 +0100

    Metodo A implementado.

---
+++ WebAPI/WebAPI/Controllers/WebApisController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class WebApisController : ApiController
+    {
+        // GET: api/WebApis
+
+        [Route("api/MetodoA")]
+        [HttpGet]
+        public List<PercursoFormacao> GetListPercursos()
+        {
+            List<PercursoFormacao> listaPercurso = new List<PercursoFormacao>();
+
+            SqlConnection conn = new SqlConnection("Data Source=CABARET-PC;Initial Catalog=BDApp;Integrated Security=True");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT id_percurso AS ID, nome AS [Nome], descricao AS Descrição, duracao AS Duração, competencia AS Competência FROM percurso_formativo WHERE ativo = 0", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                PercursoFormacao percurso = new PercursoFormacao();
+
+                percurso.nome = reader[1].ToString();
+                percurso.descricao = reader[2].ToString();
+                percurso.competencia = reader[4].ToString();
+
+                listaPercurso.Add(percurso);
+            }
+
+            conn.Close();
+            return listaPercurso;
+        }
+
+        // GET: api/WebApis/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/WebApis
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/WebApis/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/WebApis/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/PercursoFormacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class PercursoFormacao
+    {
+        public int id_percurso { get; set; }
+
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        public bool ativo { get; set; }
+
+        public int duracao { get; set; }
+
+        public string competencia { get; set; }
+    }
+}
